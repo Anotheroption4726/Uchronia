62b9ee433187280eaf53889e2bc86991164f8015
John <raph.en223@gmail.com>
2018-12-19 22:51:42 +0100

    zea

---
+++ Assets/Class_human.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+public class Human_class
+{
+
+    private int x;
+    private int y;
+    private int age;
+    public static List<Human_class> population;
+
+    public int X { get => x; set => x = value; }
+    public int Y { get => y; set => y = value; }
+    public int Age { get => age; set => age = value; }
+
+    Human_class(int x, int y, int age)
+    {
+        X = x;
+        Y = y;
+        Age = age;
+    }
+
+    public static int count_tile(int x, int y)
+    {
+        int result = 0;
+        for (int i = 0; i < population.Count; i++)
+        {
+            if (population[i].X == x && population[i].Y == y)
+            {
+                result++;
+            }
+        }
+        return result;
+    }
+
+    public static void start()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Human_class pop = new Human_class(0, 0, 10);
+            population.Add(pop);
+        }
+    }
+
+
+   public static void update()
+    {
+        System.Random rd = new System.Random();
+        if (rd.Next(1, 35) % 2 == 0)
+        {
+            Human_class pop = new Human_class(0, 0, 10);
+            population.Add(pop);
+        }
+        for (int i = 0; i < population.Count; i++)
+        {
+            population[i].Age++;
+            if (population[i].Age > 70)
+                population.RemoveAt(i);
+        }
+    }
+}
---
+++ Assets/main.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class main : MonoBehaviour
+{
+    public Vector3Int tile_position = new Vector3Int(0, 0, 1);
+    public Tilemap tile_map;
+    public Tile tile_skin;
+    public static int generation = 0;
+
+    void Start()
+    {
+        Human_class.population = new List<Human_class>();
+        Human_class.start();
+
+    }
+
+    void Update()
+    {
+        Thread.Sleep(2000);
+
+         if (Human_class.count_tile(0, 0) == 20)
+         {
+             tile_map.SetTile(tile_position, tile_skin);
+         }
+        Human_class.update();
+        //tile_map.SetTile(tile_position, tile_skin);
+        print(generation);
+        print(Human_class.population.Count);
+        generation++;
+    }
+
+
+}
